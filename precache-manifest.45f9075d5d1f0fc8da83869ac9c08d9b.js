f2336888eaddb5c60a36d3deb011ec7caff7dcab
Xin Liu <xinliu253@yahoo.com>
2019-12-21 00:59:34 -0500

    Updates

---
+++ precache-manifest.45f9075d5d1f0fc8da83869ac9c08d9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef6db5532b5e0e9dd720f56c2c3299e",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "4cb6bdcbd987f80ad8e0",
+    "url": "/myapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cc56ca433aa9add4c52e",
+    "url": "/myapp/static/js/2.36767d31.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myapp/static/js/2.36767d31.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4cb6bdcbd987f80ad8e0",
+    "url": "/myapp/static/js/main.a001276e.chunk.js"
+  },
+  {
+    "revision": "4c5d71b77e57f8b73954",
+    "url": "/myapp/static/js/runtime-main.16e32a26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
